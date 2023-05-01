4eec7d00e7cd5a6fb1b247fbb1415ad0e58ba4c2
Robert Smith <thersmith09@gmail.com>
2023-04-30 23:04:57 -0700

    Added placeholder text

--- preload.js
+++ preload.js
@@ -3,8 +3,4 @@ window.addEventListener('DOMContentLoaded', () => {
         const element = document.getElementById(selector)
         if (element) element.innerText = text
     }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependency}-version`, process.versions[dependency])
-    }
 })
